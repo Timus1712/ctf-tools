911cb256c07949c68637845ebd8ba8c8522928bf
Timofey Yushchenko <TYushchenko@croc.ru>
2015-11-18 20:45:20 +0300

    put check ip in parallelize

--- helper.py
+++ helper.py
@@ -11,28 +11,20 @@ MAX_TEAM_NUMBER = 312
 
 DEVNULL = open(os.devnull, 'wb')
 
-def getIpList():
-	l = []
-	for i in range(1, MAX_TEAM_NUMBER + 1):
-		l.append("10." + str(60 + i / 255) + "." + str(i % 255) + ".3")
-	return l
+
+def ips_generator():
+	i = 1
+	while True:
+		ip = "10." + str(60 + i / 255) + "." + str(i % 255) + ".3"
+		yield (ip, i)
+		i += 1
+		if i == MAX_TEAM_NUMBER + 1:
+			i = 1
 
 
 def check_ip(ip):
 	response = subprocess.call(["ping", "-c 1", "-W 1", ip], stdout=DEVNULL, stderr=subprocess.STDOUT)
 	return response == 0
-
-
-def chooseGoodIp(ips):
-	cur = 0
-	while True:
-		ip = ips[cur]
-		team_id = cur + 1
-		if check_ip(ip):
-			yield (ip, team_id)
-		cur += 1
-		if cur == len(ips):
-			cur = 0
 
 
 def submit(flag):
@@ -42,8 +34,9 @@ def submit(flag):
 
 
 def parallelize_wrapper(func, q, ip, team_id):
-	flag = func(ip, team_id)
-	q.put(flag)
+	if check_ip(ip):
+		flag = func(ip, team_id)
+		q.put(flag)
 
 
 def parallelize_sender(q):
@@ -51,7 +44,7 @@ def parallelize_sender(q):
 		flag = q.get()
 		if flag == None:
 			break
-		if flag is str:
+		if type(flag) == str:
 			print "Send ", flag
 			submit(flag)
 		else:
@@ -62,13 +55,12 @@ def parallelize_sender(q):
 
 def parallelize(flag_getter, threads=5):
 	try:
-		ips = getIpList()
 		m = Manager()
 		p = Pool(threads)
 		q = m.Queue()
 		submitter = Process(target=parallelize_sender, args=(q,))
 		submitter.start()
-		for ip, team_id in chooseGoodIp(ips):
+		for ip, team_id in ips_generator():
 #			print "stealing flag from {1} with ip: {0}".format(ip, team_id)
 			p.apply_async(parallelize_wrapper, args=(flag_getter, q, ip, team_id,))
 	except KeyboardInterrupt:

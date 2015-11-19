a48a03ceebf8242af9604a89cf8f37eab97c7be7
Timofey Yushchenko <TYushchenko@croc.ru>
2015-11-19 15:08:04 +0300

    remove timestamp field from db

--- flag_submitter/create_table.py
+++ flag_submitter/create_table.py
@@ -5,7 +5,7 @@ db_connection = sqlite3.connect("flags.db")
 cursor = db_connection.cursor()
 
 cursor.execute("CREATE TABLE FLAGS (FLAG varchar(255), TIMESTAMP int)")
-cursor.execute("CREATE TABLE USED (FLAG varchar(255), TIMESTAMP int)")
+cursor.execute("CREATE TABLE USED (FLAG varchar(255))")
 
 db_connection.commit()
 db_connection.close()
